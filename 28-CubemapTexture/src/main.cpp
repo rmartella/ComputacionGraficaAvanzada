9e422e923cf3254e45e8d0464df7795d66f28275
rmartella <reymar_44@hotmail.com>
2018-04-20 05:45:31 -0500

    Add the cubemap texture

---
+++ 28-CubemapTexture/src/main.cpp
@@ -0,0 +1,284 @@
+//glew include
+#include <GL/glew.h>
+
+//std includes
+#include <string>
+#include <iostream>
+
+//glfw include
+#include <GLFW/glfw3.h>
+
+// program include
+#include "Headers/InputManager.h"
+#include "Headers/TimeManager.h"
+#include "Headers/Shader.h"
+
+// Texture include
+#include "Headers/Texture.h"
+
+// Camera include
+#include "Headers/CameraFPS.h"
+
+#include "Headers/Model.h"
+
+// Sphere include
+#include "Headers/Sphere.h"
+
+Sphere sp(1.5, 50, 50, MODEL_MODE::VERTEX_COLOR);
+Sphere sp2(1.5, 50, 50, MODEL_MODE::VERTEX_LIGHT_TEXTURE);
+
+Shader lightingShader;
+Shader lampShader;
+Shader cubemapShader;
+
+Model modelo1;
+
+Texture textureDifuse(GL_TEXTURE_2D, "../Textures/container2.png");
+Texture textureSpecular(GL_TEXTURE_2D, "../Textures/container2_specular.png");
+
+GLuint VAO, VBO, EBO;
+
+int screenWidth;
+int screenHeight;
+
+GLFWwindow * window;
+InputManager inputManager;
+double deltaTime;
+
+// Se definen todos las funciones.
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod);
+void init(int width, int height, std::string strTitle, bool bFullScreen);
+void destroyWindow();
+void destroy();
+bool processInput(bool continueApplication = true);
+
+// Implementacion de todas las funciones.
+void init(int width, int height, std::string strTitle, bool bFullScreen) {
+
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		exit(-1);
+	}
+
+	screenWidth = width;
+	screenHeight = height;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	if (bFullScreen)
+		window = glfwCreateWindow(width, height, strTitle.c_str(),
+			glfwGetPrimaryMonitor(), nullptr);
+	else
+		window = glfwCreateWindow(width, height, strTitle.c_str(), nullptr,
+			nullptr);
+
+	if (window == nullptr) {
+		std::cerr
+			<< "Error to create GLFW window, you can try download the last version of your video card that support OpenGL 3.3+"
+			<< std::endl;
+		destroyWindow();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(0);
+
+	glfwSetWindowSizeCallback(window, reshapeCallback);
+	glfwSetWindowSizeCallback(window, reshapeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Init glew
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cerr << "Failed to initialize glew" << std::endl;
+		exit(-1);
+	}
+
+	glViewport(0, 0, screenWidth, screenHeight);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// Enable test depth, must be clear depth buffer bit
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+	sp.init();
+	sp.load();
+
+	sp2.init();
+	sp2.load();
+
+	modelo1.loadModel("../objects/cyborg/cyborg.obj");
+
+	lightingShader.initialize("../Shaders/loadModelLighting.vs", "../Shaders/loadModelLighting.fs");
+	lampShader.initialize("../Shaders/lampShader.vs", "../Shaders/lampShader.fs");
+	cubemapShader.initialize("../Shaders/cubemapTexture.vs", "../Shaders/cubemapTexture.fs");
+	textureDifuse.load();
+	
+}
+
+void destroyWindow() {
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void destroy() {
+	destroyWindow();
+	lightingShader.destroy();
+	lampShader.destroy();
+	cubemapShader.destroy();
+}
+
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes) {
+	screenWidth = widthRes;
+	screenHeight = heightRes;
+	glViewport(0, 0, widthRes, heightRes);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	inputManager.keyPressed(inputManager.toApplicationKey(key), deltaTime * 10.0f,
+		inputManager.toApplicationState(action));
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+	inputManager.mouseMoved(xpos, ypos);
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+	inputManager.mouseClicked(inputManager.toMouseButtonIndex(button), xpos,
+		ypos, inputManager.toApplicationState(state));
+}
+
+bool processInput(bool continueApplication) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS
+		|| glfwWindowShouldClose(window) != 0) {
+		return false;
+	}
+	TimeManager::Instance().CalculateFrameRate(false);
+	deltaTime = TimeManager::Instance().DeltaTime;
+	inputManager.do_movement(deltaTime);
+	glfwPollEvents();
+	return continueApplication;
+}
+
+void applicationLoop() {
+	bool psi = true;
+
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+	double lastTime = TimeManager::Instance().GetTime();
+
+	while (psi) {
+		psi = processInput(true);
+		// This is new, need clear depth buffer bit
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Create camera transformations
+		glm::mat4 view = inputManager.getCameraFPS()->GetViewMatrix();
+		glm::mat4 projection;
+		projection = glm::perspective(45.0f, (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+
+		/*lightingShader.turnOn();
+
+		GLint viewPosLoc = lightingShader.getUniformLocation("viewPos");
+		glUniform3f(viewPosLoc, inputManager.getCameraFPS()->Position.x, inputManager.getCameraFPS()->Position.y,
+			inputManager.getCameraFPS()->Position.z);
+
+		// Set material properties
+		GLint matDiffuseLoc = lightingShader.getUniformLocation(
+			"material.diffuse");
+		GLint matSpecularLoc = lightingShader.getUniformLocation(
+			"material.specular");
+		GLint matShineLoc = lightingShader.getUniformLocation(
+			"material.shininess");
+		glUniform1i(matDiffuseLoc, 0);
+		glUniform1i(matSpecularLoc, 1);
+		glUniform1f(matShineLoc, 32.0f);
+
+		// Set lights properties
+		GLint lightAmbientLoc = lightingShader.getUniformLocation(
+			"light.ambient");
+		GLint lightDiffuseLoc = lightingShader.getUniformLocation(
+			"light.diffuse");
+		GLint lightSpecularLoc = lightingShader.getUniformLocation(
+			"light.specular");
+		GLint lightPosLoc = lightingShader.getUniformLocation("light.position");
+		glUniform3f(lightAmbientLoc, 0.2f, 0.2f, 0.2f);
+		glUniform3f(lightDiffuseLoc, 0.5f, 0.5f, 0.5f); // Let's darken the light a bit to fit the scene
+		glUniform3f(lightSpecularLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+
+		// Get the uniform locations
+		GLint modelLoc = lightingShader.getUniformLocation("model");
+		GLint viewLoc = lightingShader.getUniformLocation("view");
+		GLint projLoc = lightingShader.getUniformLocation("projection");
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Draw a sphere
+		glm::mat4 model;
+		model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		textureDifuse.bind(GL_TEXTURE0);
+
+		sp2.render();
+		//modelo1.render(&lightingShader);
+		lightingShader.turnOff();
+
+		lampShader.turnOn();
+		// Create transformations
+		modelLoc = lampShader.getUniformLocation("model");
+		viewLoc = lampShader.getUniformLocation("view");
+		projLoc = lampShader.getUniformLocation("projection");
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		model = glm::translate(glm::mat4(), lightPos);
+		model = glm::scale(model, glm::vec3(0.2, 0.2, 0.2));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		sp.render();
+		lampShader.turnOff();*/
+
+		cubemapShader.turnOn();
+
+		GLint viewLoc = cubemapShader.getUniformLocation("view");
+		GLint projLoc = cubemapShader.getUniformLocation("projection");
+		GLint modelLoc = cubemapShader.getUniformLocation("model");
+
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		
+		glm::mat4 cubeModel;
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(cubeModel));
+
+		textureDifuse.bind(GL_TEXTURE0);
+		GLint unitTexture = cubemapShader.getUniformLocation("texture0");
+		glUniform1i(unitTexture, 0);
+
+		sp2.render();
+
+		cubemapShader.turnOff();
+
+		glfwSwapBuffers(window);
+	}
+}
+
+int main(int argc, char ** argv) {
+	init(800, 700, "Window GLFW", false);
+	applicationLoop();
+	destroy();
+	return 1;
+}
+
