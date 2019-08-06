7fcd134f1528e84f634c9a71ab1ed9da75970f82
rmartella <reymar_44@hotmail.com>
2019-08-06 09:59:26 -0500

    Add the config proyects to Linux

---
+++ 02-BuffersVBO/src/main.cpp
@@ -0,0 +1,151 @@
+//glew include
+#include <GL/glew.h>
+
+//std includes
+#include <string>
+#include <iostream>
+
+//glfw include
+#include <GLFW/glfw3.h>
+
+// program include
+#include "Headers/TimeManager.h"
+
+int screenWidth;
+int screenHeight;
+
+GLFWwindow * window;
+
+bool exitApp = false;
+int lastMousePosX;
+int lastMousePosY;
+
+double deltaTime;
+
+// Se definen todos las funciones.
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod);
+void init(int width, int height, std::string strTitle, bool bFullScreen);
+void destroy();
+bool processInput(bool continueApplication = true);
+
+// Implementacion de todas las funciones.
+void init(int width, int height, std::string strTitle, bool bFullScreen) {
+	
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		exit(-1);
+	}
+
+	screenWidth = width;
+	screenHeight = height;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	if (bFullScreen)
+		window = glfwCreateWindow(width, height, strTitle.c_str(),
+			glfwGetPrimaryMonitor(), nullptr);
+	else
+		window = glfwCreateWindow(width, height, strTitle.c_str(), nullptr,
+			nullptr);
+
+	if (window == nullptr) {
+		std::cerr
+			<< "Error to create GLFW window, you can try download the last version of your video card that support OpenGL 3.3+"
+			<< std::endl;
+		destroy();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(0);
+
+	glfwSetWindowSizeCallback(window, reshapeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Init glew
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cerr << "Failed to initialize glew" << std::endl;
+		exit(-1);
+	}
+
+	glViewport(0, 0, screenWidth, screenHeight);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+}
+
+void destroy() {
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void reshapeCallback(GLFWwindow* Window, int widthRes, int heightRes) {
+	screenWidth = widthRes;
+	screenHeight = heightRes;
+	glViewport(0, 0, widthRes, heightRes);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (action == GLFW_PRESS) {
+		switch (key) {
+		case GLFW_KEY_ESCAPE:
+			exitApp = true;
+			break;
+		}
+	}
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+	lastMousePosX = xpos;
+	lastMousePosY = ypos;
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
+	if (state == GLFW_PRESS) {
+		switch (button) {
+		case GLFW_MOUSE_BUTTON_RIGHT:
+			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			break;
+		case GLFW_MOUSE_BUTTON_LEFT:
+			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			break;
+		case GLFW_MOUSE_BUTTON_MIDDLE:
+			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			break;
+		}
+	}
+}
+
+bool processInput(bool continueApplication){
+	if (exitApp || glfwWindowShouldClose(window) != 0) {
+		return false;
+	}
+	deltaTime = 1 / TimeManager::Instance().CalculateFrameRate(false);
+	glfwPollEvents();
+	return continueApplication;
+}
+
+void applicationLoop() {
+	bool psi = true;
+	while (psi) {
+		psi = processInput(true);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+}
+
+int main(int argc, char ** argv) {
+	init(800, 700, "Window GLFW", false);
+	applicationLoop();
+	destroy();
+	return 1;
+}
