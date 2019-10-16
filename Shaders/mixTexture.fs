f01735a3b16b6cd6678eb5e154db9374b3b038da
rmartella <reymar_44@hotmail.com>
2019-10-16 01:09:53 -0500

    Add the example 12

--- Shaders/mixTexture.fs
+++ Shaders/mixTexture.fs
@@ -1,13 +1,10 @@
 #version 330 core
-in vec3 ex_Color;
-in vec2 ex_texCoord;
 
+in vec2 out_uv;
 out vec4 color;
-
 uniform sampler2D textura1;
 uniform sampler2D textura2;
 
-void main()
-{
-	color = mix(texture(textura1, ex_texCoord), texture(textura2, ex_texCoord), 0.3);
+void main(){
+	color = mix(texture(textura1, out_uv), texture(textura2, out_uv), 0.3);
 }
