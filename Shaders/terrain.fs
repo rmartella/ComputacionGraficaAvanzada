014203dc455393e5688e3786b366e4448c7b6d44
rmartella <reymar_44@hotmail.com>
2020-02-23 22:38:04 -0600

    Terrain comment example

--- Shaders/terrain.fs
+++ Shaders/terrain.fs
@@ -57,23 +57,26 @@ uniform vec3 viewPos;
 uniform vec2 scaleUV;  
   
 uniform sampler2D backgroundTexture;
-uniform sampler2D rTexture;
+/*uniform sampler2D rTexture;
 uniform sampler2D gTexture;
 uniform sampler2D bTexture;
-uniform sampler2D blendMapTexture;
+uniform sampler2D blendMapTexture;*/
 
 vec3 calculateDirectionalLight(Light light, vec3 direction){
 	vec2 tiledCoords = our_uv;
 	if(tiledCoords.x != 0 && tiledCoords.y != 0)
 		tiledCoords = scaleUV * tiledCoords;
 
-	vec4 blendMapColor = texture(blendMapTexture, our_uv);
+	/*vec4 blendMapColor = texture(blendMapTexture, our_uv);
 	float backTextureAmount = 1 - (blendMapColor.r + blendMapColor.g + blendMapColor.b);
 	vec4 backgroundTextureColor = texture(backgroundTexture, tiledCoords) * backTextureAmount;
 	vec4 rTextureColor = texture(rTexture, tiledCoords) * blendMapColor.r;
 	vec4 gTextureColor = texture(gTexture, tiledCoords) * blendMapColor.g;
 	vec4 bTextureColor = texture(bTexture, tiledCoords) * blendMapColor.b;
-	vec4 totalColor = backgroundTextureColor + rTextureColor + gTextureColor + bTextureColor;
+	vec4 totalColor = backgroundTextureColor + rTextureColor + gTextureColor + bTextureColor;*/
+	
+	vec4 backgroundTextureColor = texture(backgroundTexture, tiledCoords);
+	vec4 totalColor = backgroundTextureColor;
 
 	// Ambient
     vec3 ambient  = light.ambient * vec3(totalColor);
